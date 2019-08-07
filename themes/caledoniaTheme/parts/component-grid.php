9cf34b078bf9f03c36f1071828512bd878a7e573
Clarisa Angaramo <clarisa.angaramo@bladonmore.com>
2019-08-07 16:00:59 +0100

    Flexible grid

--- themes/caledoniaTheme/parts/component-grid.php
+++ themes/caledoniaTheme/parts/component-grid.php
@@ -1,116 +1,107 @@
 <?php
-    $film = $values['film'];
-    $quote = $values['quote'];
-    $number = $values['number'];
-    $page = $values['page'];
-    $page2 = $values['page_2'];
-    $image = $values['image'];
+    $items = $values['items'];
 ?>
 <div class="container grid my-4 my-lg-5">
 
     <div class="row">
 
-        <div class="col-lg-8 px-lg-1 my-1 overflow-hidden">
+        <?php foreach ($items as $item): 
 
-            <div class="box w-100 bigger linked-box position-relative" style="overflow:hidden" data-video="87110435">
+            $layout = $item['acf_fc_layout'];
 
-                <!-- Video -->
-                <div class="video">
-                    <div class='embed-container'>
-                        <iframe src='https://player.vimeo.com/video/87110435?title=0&byline=0&portrait=0' allow="autoplay" frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-                    </div>
-                </div>
+            /* Film */
+            if ($layout == "film"): ?>
 
-                <!-- Thumbnail -->
-                <div class="video-thumbnail w-100 h-100">
+                <div class="col-lg-8 px-lg-1 my-1 overflow-hidden">
 
-                    <div class="w-100 h-100 overflow-hidden">
-                        <div class="w-100 h-100 bg-image zoom" style="background-image: url(<?php echo $film['background_image'] ?>)"></div>
-                    </div>
+                    <div class="box w-100 bigger linked-box position-relative" style="overflow:hidden" data-video="<?php echo $item['video_id'] ?>">
 
-                    <div class="light-layer w-100 h-100">
-                        <div class="h-100 d-flex align-items-center justify-content-center text-center">
-                            <div>
-                                <img class="my-3 play-film open-video" src="<?php echo get_bloginfo('template_directory'); ?>/resources/play.svg">
-                                <h2 class="mb-4 pb-md-3 color-white overview">Overview film</h2>
-                                <a class="link open-video">Watch film</a>
+                        <!-- Video -->
+                        <div class="video">
+                            <div class='embed-container'>
+                                <iframe src='https://player.vimeo.com/video/<?php echo $item['video_id'] ?>?title=0&byline=0&portrait=0' allow="autoplay" frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
                             </div>
                         </div>
-                    </div> 
-                </div>
 
-            </div>
+                        <!-- Thumbnail -->
+                        <div class="video-thumbnail w-100 h-100">
 
-        </div>
-                            
-        <!-- Quote -->
-        <div class="col-lg-4 px-lg-1 px-md-2 my-1">
-            <div class="box quote-box p-4 bigger">
-                <p class="quote"><?php echo $quote['quote'] ?></p>
-                <p class="author"><?php echo $quote['author'] ?></p>
-            </div>
-        </div>
-        
-        <!-- Number -->
-        <div class="col-lg-4 px-lg-1 my-1">
-            <div class="box w-100 number px-5 py-4">
-                <span><?php echo $number['symbol'] ?></span><span class="counter"><?php echo $number['number'] ?></span><span><?php echo $number['value'] ?></span>
-                <p class="description"><?php echo $number['description'] ?></p>
-            </div>
-        </div>
+                            <div class="w-100 h-100 overflow-hidden">
+                                <div class="w-100 h-100 bg-image zoom" style="background-image: url(<?php echo $item['background_image'] ?>)"></div>
+                            </div>
 
-        <!-- Page -->
-        <div class="col-lg-8 px-lg-1 my-1">
-            <div class="box position-relative linked-box" data-link="<?php echo $page['link'] ?>">
-
-                <!-- Image -->
-                <div class="w-100 h-100 overflow-hidden">
-                    <div class="w-100 h-100 bg-image zoom" style="background-image: url(<?php echo $page['image'] ?>)"></div>
-                </div>
-
-                <!-- Layer -->
-                <div class="light-layer w-100 h-100">
-                    <div class="h-100 d-flex align-items-center justify-content-center text-center">
-                        <div>
-                            <h2 class="mb-2 color-white"><?php echo $page['title'] ?></h2>
-                            <p class="introduction mb-5"><?php echo $page['introduction'] ?></p>
-                            <a class="link" href="<?php echo $page['link'] ?>"><?php echo $page['button_label'] ?></a>
+                            <div class="light-layer w-100 h-100">
+                                <div class="h-100 d-flex align-items-center justify-content-center text-center">
+                                    <div>
+                                        <img class="my-3 play-film open-video" src="<?php echo get_bloginfo('template_directory'); ?>/resources/play.svg">
+                                        <h2 class="mb-4 pb-md-3 color-white overview">Overview film</h2>
+                                        <a class="link open-video">Watch film</a>
+                                    </div>
+                                </div>
+                            </div> 
                         </div>
+
                     </div>
                 </div>
 
-            </div>
-        </div>
+            <!-- Quote -->
+            <?php elseif ($layout == "quote"): ?>
 
-        <!-- Page -->
-        <div class="col-lg-8 px-lg-1 my-1">
-            <div class="box position-relative linked-box" data-link="<?php echo $page2['link'] ?>">
-
-                <!-- Image -->
-                <div class="w-100 h-100 overflow-hidden">
-                    <div class="w-100 h-100 bg-image zoom" style="background-image: url(<?php echo $page2['image'] ?>)"></div>
-                </div>
-
-                <!-- Layer -->
-                <div class="light-layer w-100 h-100">
-                    <div class="h-100 d-flex align-items-center justify-content-center text-center">
-                        <div>
-                            <h2 class="mb-2 color-white"><?php echo $page2['title'] ?></h2>
-                            <p class="introduction mb-5"><?php echo $page2['introduction'] ?></p>
-                            <a class="link" href="<?php echo $page2['link'] ?>"><?php echo $page2['button_label'] ?></a>
-                        </div>
+                <div class="col-lg-4 px-lg-1 px-md-2 my-1">
+                    <div class="box quote-box p-4 bigger">
+                        <p class="quote"><?php echo $item['quote'] ?></p>
+                        <p class="author"><?php echo $item['author'] ?></p>
                     </div>
                 </div>
-                
-            </div>
-        </div>
 
-        <!-- Image -->
-        <div class="col-lg-4 px-lg-1 my-1">
-            <div class="box">
-                <div class="w-100 h-100 bg-image" style="background-image: url(<?php echo $image ?>)">
-            </div>
-        </div>
+            <!-- Number -->
+            <?php elseif ($layout == "number"): ?>
+
+                <div class="col-lg-4 px-lg-1 my-1">
+                    <div class="box w-100 number px-5 py-4">
+                        <span><?php echo $item['symbol'] ?></span><span class="counter"><?php echo $item['number'] ?></span><span><?php echo $item['value'] ?></span>
+                        <p class="description"><?php echo $item['description'] ?></p>
+                    </div>
+                </div>
+
+            <!-- Page -->
+            <?php elseif ($layout == "page"): ?>
+
+                <div class="col-lg-8 px-lg-1 my-1">
+                    <div class="box position-relative linked-box" data-link="<?php echo $item['link'] ?>">
+
+                        <!-- Image -->
+                        <div class="w-100 h-100 overflow-hidden">
+                            <div class="w-100 h-100 bg-image zoom" style="background-image: url(<?php echo $item['image'] ?>)"></div>
+                        </div>
+
+                        <!-- Layer -->
+                        <div class="light-layer w-100 h-100">
+                            <div class="h-100 d-flex align-items-center justify-content-center text-center">
+                                <div>
+                                    <h2 class="mb-2 color-white"><?php echo $item['title'] ?></h2>
+                                    <p class="introduction mb-5"><?php echo $item['introduction'] ?></p>
+                                    <a class="link" href="<?php echo $item['link'] ?>"><?php echo $item['button_label'] ?></a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            <!-- Image -->
+            <?php elseif ($layout == "image"): ?>
+
+                <div class="col-lg-4 px-lg-1 my-1">
+                    <div class="box">
+                        <div class="w-100 h-100 bg-image" style="background-image: url(<?php echo $item['image'] ?>)">
+                    </div>
+                </div>
+
+            <?php endif; ?>
+
+        <?php endforeach ?>
+
 
     </div>
 </div>
